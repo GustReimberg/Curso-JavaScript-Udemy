895e7e2c5b8fda62dbeb6c4edfa986727e5c068e
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-02 14:41:15 -0300

    17 - Variaveis

---
+++ 17_Variaveis/script.js
@@ -0,0 +1,20 @@
+let nome = "Matheus";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "João";
+
+console.log(nome);
+
+laranjas = 8200;
+
+console.log(laranjas);
+
+laranjas = "Laranja";
+
+console.log(laranjas);
