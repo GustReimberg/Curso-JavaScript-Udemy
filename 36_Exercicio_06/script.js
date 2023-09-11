37299635bee0ffe8e7fdeb3ef0c874e41d33cc25
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-11 18:39:26 -0300

    36 Exercicio - 06

---
+++ 36_Exercicio_06/script.js
@@ -0,0 +1,12 @@
+// Armazene em valores em variáveis com cada um dos 
+// tipos de dados vistos;
+
+// String, number e Boolean;
+
+let numero = 545;
+let stringExercicio = ("Olá!");
+let booleanExercicio = (4 > 3);
+
+console.log(typeof numero);
+console.log(typeof stringExercicio);
+console.log(typeof booleanExercicio);
