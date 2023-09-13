7ed91bbc5eab04e4ec9833b411a27607b8c1448c
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-13 18:35:07 -0300

    38 Exercicio - 08

---
+++ 38_exercicio_08/script.js
@@ -0,0 +1,12 @@
+// Crie uma constante com o seu nome como valor; 
+// Depois uma estrutura if que verifica se o seu nome é o que está na 
+// constante;
+// Se estiver, emita uma mensagem de saudação com console.log();
+
+const meuNome = "Gustavo";
+
+if(meuNome == "Gustavo") {
+    console.log("Olá Gustavo, bem vindo!");
+} else {
+    console.log("Você não é o Gustavo");
+}
