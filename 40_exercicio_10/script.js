dacd1307b0ee62f0fd73b735b4a7faf3bb837349
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-13 18:53:33 -0300

    40 Exericio - 10

---
+++ 40_exercicio_10/script.js
@@ -0,0 +1,13 @@
+// Armazene a velocidade de um carro em uma variável, com o número
+// que desejar;
+// Faça uma estrutura if/else que verifica se ele está acima da velocidade;
+// 80 é velocidade máxima perimitida;
+// Se estiver acima ou abaixo exiba mensagens com console.log;
+
+let velocidadeMaxima = 80
+
+if(velocidadeMaxima == 80){
+    console.log("Você está na velocidade máxima permitida");
+} else{
+    console.log("Você ultrapassou a velocidade máxima perimitida!");
+}
