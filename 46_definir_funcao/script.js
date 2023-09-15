8773768e3bdf7d680fd067d16d7a3cb7458369fa
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-15 16:58:48 -0300

    46 Definir uma função

---
+++ 46_definir_funcao/script.js
@@ -0,0 +1,22 @@
+function imprimirNoConsole() {
+    console.log("Olá mundo!");
+
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log(`O número é ${num}`);
+}
+
+imprimirUmNumero(10);
+imprimirUmNumero(15);
+imprimirUmNumero(20);
+imprimirUmNumero(25);
+
+const numeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
