712ec32775a2324ac8051ed186d6d26b8e579197
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-27 16:15:28 -0300

    60 - Exercicio 19

---
+++ 60_exercicio_19/script.js
@@ -0,0 +1,8 @@
+function numAleatorio(x){
+    return Math.floor(Math.random() * x) +1;
+}
+
+console.log(numAleatorio(10));
+console.log(numAleatorio(50));
+console.log(numAleatorio(100));
+console.log(numAleatorio(1000));
