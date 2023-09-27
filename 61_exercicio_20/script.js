04c6d5538cd30c7e260318a9cfbdb653fe50f537
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-09-27 16:55:59 -0300

    61 Exercicio - 20

---
+++ 61_exercicio_20/script.js
@@ -0,0 +1,13 @@
+function checkIdade(idade){
+    
+    if(idade >= 18){
+        console.log("Você tem a necessaria para entrar na auto escola");
+    } else{
+        console.log("Você não tem a idade minima necessaria para entrar na auto escola");
+
+    }
+
+}
+
+checkIdade(17);
+checkIdade(18);
