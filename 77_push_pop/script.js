d4105618fd2bfbadeb6d037ce5c4b73b7c108293
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-08 17:34:50 -0300

    push e pop

---
+++ 77_push_pop/script.js
@@ -0,0 +1,10 @@
+let nomes = ["Gustavo", "Luiz", "Gabriella"];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.push('Pauli');
+
+console.log(nomes);
