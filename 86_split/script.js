566a49ebacc336e1952b14197d031a39d10d619a
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-09 19:41:36 -0300

    86 - Split

---
+++ 86_split/script.js
@@ -0,0 +1,11 @@
+let frase = "Testando o método split";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let produtos = "Banana;Maca;Jaca;Pera;Bola;Tapete";
+
+console.log(produtos.split(';'));
