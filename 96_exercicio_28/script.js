b9fbc00cbd696fc95b3713c398272be79efcea77
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-13 17:10:22 -0300

    96 Exercicio 28

---
+++ 96_exercicio_28/script.js
@@ -0,0 +1,10 @@
+let onibus = {
+    rodas: 8,
+    limte_de_passageiros: 40,
+    portas: 2
+};
+
+
+console.log(onibus.limte_de_passageiros);
+console.log(onibus.portas);
+console.log(onibus.portas);
