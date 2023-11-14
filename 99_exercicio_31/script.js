0c6ce06da2d7b5ef31c58f784232302d110600f3
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-14 17:50:58 -0300

    95 Exercicio 31

---
+++ 99_exercicio_31/script.js
@@ -0,0 +1,19 @@
+let arrUm = [1,10,15,23,24,30];
+
+let arrDois = [2,10,14,23];
+
+
+function verificarArray(arr) {
+
+    if(arr.length > 5){
+        console.log("Muitos elementos");
+    } else{
+        console.log("Poucos Elementos")
+    }
+
+}
+
+
+verificarArray(arrUm);
+
+verificarArray(arrDois);
