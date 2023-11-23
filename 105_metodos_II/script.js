d90313b76e05076cf0538e670a30e949adfb1ff9
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-23 17:30:12 -0300

    105 Metodos 2

---
+++ 105_metodos_II/script.js
@@ -0,0 +1,24 @@
+const cachorro = {
+    raca: 'SRD',
+    uivar: function() {
+        console.log('Auuuuuuu');
+    },
+    rosnar: function() {
+        console.log('Grrrrr');
+    },
+    setRaca: function(raca) {
+        this.raca = raca;
+    },
+    getRaca: function() {
+        return "A raça é" , this.raca;
+    }
+}
+
+console.log(cachorro.raca);
+
+cachorro.setRaca('Pastor alemão');
+
+console.log(cachorro.raca);
+
+
+console.log(cachorro.getRaca());
