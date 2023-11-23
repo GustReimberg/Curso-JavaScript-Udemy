86ec6be472344d9be0a0076705551c1d8d6afbdc
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-23 17:19:02 -0300

    104 Metodos Objetos

---
+++ 104_metodos_objetos/script.js
@@ -0,0 +1,11 @@
+const cachorro ={
+    uivar: function (){
+        console.log("AUUUUUUUUUUU");
+    },
+    rosnar: function(){
+        console.log("grrrrr");
+    }
+}
+
+cachorro.uivar();
+cachorro.rosnar();
