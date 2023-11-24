9921ab2ed825f26573b5a97546ddb2c4f252240d
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-24 18:25:41 -0300

    109 Instanciando Classes

---
+++ 109_instanciando_classes/script.js
@@ -0,0 +1,20 @@
+function criarCachorro(raca, patas, cor){
+    let cachorro = Object.create({});
+    cachorro.raca = raca;
+    cachorro.patas = patas;
+    cachorro.cor = cor;
+    cachorro.latir = function(){
+        console.log("Au Au");
+    }
+    return cachorro;
+}
+
+let doberman = criarCachorro('Doberman', 4, 'preta');
+
+console.log(doberman);
+doberman.latir();
+
+
+let labrador = criarCachorro('Labrador',4,'Branco');
+
+console.log(labrador);
