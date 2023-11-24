f36b336dd80c17d1395b5a92c028dad05de25602
GustReimberg <143562955+GustReimberg@users.noreply.github.com>
2023-11-24 18:44:21 -0300

    111 - Metodos Prototypes

---
+++ 111_metodos_prototype/script.js
@@ -0,0 +1,18 @@
+function Cachorro(raca,pata,cor){
+    this.pata = pata;
+    this.cor = cor;
+    this.raca = raca;
+}
+
+Cachorro.prototype.uivar = function(){
+    console.log("Auuuuu");
+}
+
+Cachorro.prototype.latir = function(){
+    console.log("Au Au")
+}
+
+let husky = new Cachorro('Husky', 4,'cinza');
+
+husky.uivar();
+husky.latir();
